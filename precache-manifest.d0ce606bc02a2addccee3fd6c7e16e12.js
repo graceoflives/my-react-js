031e1b45c5fb8afc56f29e1208dbea7bbb3737f0
ngocbt <tienngocbui2612@gmail.com>
2019-04-02 10:42:30 +0700

    Updates

---
+++ precache-manifest.d0ce606bc02a2addccee3fd6c7e16e12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c249ee158f465faa4ca",
+    "url": "/my-react-js/static/js/runtime~main.c7ea762d.js"
+  },
+  {
+    "revision": "482adb3613672a3beac1",
+    "url": "/my-react-js/static/js/main.1a1cb62a.chunk.js"
+  },
+  {
+    "revision": "2d7475b355cc096cab8e",
+    "url": "/my-react-js/static/js/2.8726b323.chunk.js"
+  },
+  {
+    "revision": "482adb3613672a3beac1",
+    "url": "/my-react-js/static/css/main.a705a735.chunk.css"
+  },
+  {
+    "revision": "3463082c6aac9ef52d63935432d3bf38",
+    "url": "/my-react-js/index.html"
+  }
+];
